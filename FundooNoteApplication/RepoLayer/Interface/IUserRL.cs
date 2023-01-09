5ed9e485e689d5589e4b5e081a6a7a23ea915b34
siddharth gandhi <siddharthgandhi@outlook.com>
2023-01-09 14:31:46 +0530

    [Siddharth]Add:Add UserRL,UserRegistertion

---
+++ FundooNoteApplication/RepoLayer/Interface/IUserRL.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.ModelClass;
+using RepoLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoLayer.Interface
+{
+    public interface IUserRL
+    {
+        public UserEntity Registration(UserRegistation userRegistation);
+    }
+}
---
+++ FundooNoteApplication/RepoLayer/Service/UserRL.cs
@@ -0,0 +1,46 @@
+﻿using CommonLayer.ModelClass;
+using RepoLayer.Context;
+using RepoLayer.Entity;
+using RepoLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoLayer.Service
+{
+    public class UserRL: IUserRL
+    {
+        FundooContext fundooContext;
+        public UserRL(FundooContext fundooContext)
+        {
+            this.fundooContext = fundooContext;
+        }
+
+        public UserEntity Registration (UserRegistation userRegistation)
+        {
+            try
+            {
+                UserEntity userEntity = new UserEntity();
+                userEntity.FirstName= userRegistation.FirstName;
+                userEntity.LastName= userRegistation.LastName;
+                userEntity.Email= userRegistation.Email;
+                userEntity.Password= userRegistation.Password;
+                fundooContext.UserTable.Add(userEntity);
+                int result = fundooContext.SaveChanges();
+                if (result>0)
+                {
+                    return userEntity;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
